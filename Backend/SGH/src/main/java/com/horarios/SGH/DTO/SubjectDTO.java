87c33738bfc90b20665edff813be99cae65033be
martin <martinstibennarvaez2006@gmail.com>
2025-09-17 08:54:35 -0500

    feat(Backend)

--- Backend/SGH/src/main/java/com/horarios/SGH/DTO/SubjectDTO.java
+++ Backend/SGH/src/main/java/com/horarios/SGH/DTO/SubjectDTO.java
@@ -1,7 +1,13 @@
 package com.horarios.SGH.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SubjectDTO {
     private int subjectId;
+
+    @NotBlank(message = "El nombre de la materia no puede estar vacío")
+    @Size(min = 4, max = 18, message = "El nombre de la materia debe tener entre 4 y 18 caracteres")
     private String subjectName;
 
     public int getSubjectId() { return subjectId; }
--- Backend/SGH/src/main/java/com/horarios/SGH/DTO/TeacherDTO.java
+++ Backend/SGH/src/main/java/com/horarios/SGH/DTO/TeacherDTO.java
@@ -2,7 +2,6 @@ package com.horarios.SGH.DTO;
 
 import java.util.List;
 
-import com.horarios.SGH.Model.subjects;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
